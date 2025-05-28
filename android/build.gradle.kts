f6331b009520d599c88764a700e2b60692825a61
Th9o <theofilusalva@gmail.com>
2025-05-29 01:39:55 +0900

    coba konekkan dgn firebase
    mencoba konekkan dgn firebase, download file dari firebase trus masukan di folder android/app

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,3 +1,15 @@
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        // Wajib: Plugin Android Gradle & Google Services
+        classpath("com.android.tools.build:gradle:8.2.0")
+        classpath("com.google.gms:google-services:4.4.2")
+    }
+}
+
 allprojects {
     repositories {
         google()
@@ -5,17 +17,21 @@ allprojects {
     }
 }
 
-val newBuildDir: Directory = rootProject.layout.buildDirectory.dir("../../build").get()
-rootProject.layout.buildDirectory.value(newBuildDir)
+// (Opsional) Ubah lokasi build output jika perlu, bisa dihapus jika tidak digunakan
+val newBuildDir = rootProject.layout.buildDirectory.dir("../../build").get()
+rootProject.layout.buildDirectory.set(newBuildDir)
 
 subprojects {
-    val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
-    project.layout.buildDirectory.value(newSubprojectBuildDir)
+    val newSubprojectBuildDir = newBuildDir.dir(project.name)
+    project.layout.buildDirectory.set(newSubprojectBuildDir)
 }
+
+// Wajib: Pastikan subproject app dikonfigurasi duluan
 subprojects {
     project.evaluationDependsOn(":app")
 }
 
+// Task clean
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }
